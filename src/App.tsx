730c2b264f29cc0a18f8c30dc41715b7617e5c73
Joe Lanzon <joelanzon@outlook.com>
2024-02-25 20:41:27 +0000

    dev notice

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,13 @@ function App() {
   return (
     <>
       <div className="min-h-screen w-screen">
-        <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <MaxWidthWrapper className="mb-12 mt-18 sm:mt-20 flex flex-col items-center justify-center text-center">
+          <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border dark:border-gray-600 border-gray-200 bg-white dark:bg-slate-600 px-7 py-2 shadow-md backdrop-blur transition-all  hover:border-gray-300 hover:bg-white/50">
+            <p className="text-sm font-semibold dark:text-white text-gray-700">
+              My portfolio is currently{" "}
+              <span className="text-red-600">UNDER CONTRUCTION</span>
+            </p>
+          </div>
           <div>
             <h1 className="max-w-4xl text-4xl font-bold md:text-5xl lg:text-7xl">
               My name is
