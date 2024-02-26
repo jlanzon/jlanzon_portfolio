6e6f2fed02c4dfbfd309fb57d4907c4729d2d868
Joe Lanzon <joelanzon@outlook.com>
2024-02-26 22:31:04 +0000

    okay.... fixed with a '_'

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -113,7 +113,7 @@ export const BackgroundBeams = ({ className }: { className?: string }) => {
           ></motion.path>
         ))}
         <defs>
-          {paths.map((path, index) => (
+          {paths.map((_path, index) => (
             <motion.linearGradient
               id={`linearGradient-${index}`}
               x1="100%"
